ac19a5e1e8c88b2a036dd7bf8e39a7a5a4fc9755
Burhan Bilen <34451356+burhanbilen@users.noreply.github.com>
2022-03-05 06:35:41 +0300

    Update Question5.py

--- Question5.py
+++ Question5.py
@@ -10,16 +10,20 @@ print(null_count)
 
 country_records = df.country.value_counts() # Count the number of records for each country
 groupped_records = df.groupby('country')['daily_vaccinations'] # Group all daily vaccinations by the 'country' column
+invalid_countries = [i[0] for i in pd.Series(groupped_records) if i[1].isna().sum() == len(i[1])] # Get all countries that have invalid records
 
 for record in groupped_records: # Iterate over groupped records   
-    if country_records[record[0]] < 2: # if the number of records is just 1, that means no valid records for that country
+    if record[0] in invalid_countries: # if all daily vaccination values of the current country are NaN, that means no valid records for that country
         df.loc[(df['country'] == record[0]) & (df.daily_vaccinations.isna()), 'daily_vaccinations'] = 0.0
     df.loc[(df['country'] == record[0]) & (df.daily_vaccinations.isna()), 'daily_vaccinations'] = groupped_records.min()[record[0]]
-
+    
     """ if the country matches the index - in .loc[] section - which is called "record", and if it has nan values,
     change it by the minimum value of the current index(country) """
 
-#df.to_csv("ImputedData.csv", encoding="utf-8", index=False) # Save the new imputed dataset
+df.to_csv("ImputedData.csv", encoding="utf-8", index=False) # Save the new imputed dataset
 
-top_three = df.groupby('country')['daily_vaccinations'].median().sort_values()[::-1][:3] # Group countries by daily vaccinations using their medians, then sort values and then reverse the output to get first three (top) values.
+top_three = df.groupby('country')['daily_vaccinations'].median().sort_values()[::-1][:3] # Group countries by daily vaccinations by their medians, then sort values and then reverse output to get first three(top) values.
 print(top_three)
+
+#total_on_date = df.groupby('date')['daily_vaccinations'].sum() # Get maximum vaccinations per dates
+#print(total_on_date['1/6/2021']) # Print the amount of vaccination for the specific date
