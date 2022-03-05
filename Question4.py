a3896f3523f99ee97bb36e6f9e8446d15b1e5697
Burhan Bilen <34451356+burhanbilen@users.noreply.github.com>
2022-03-05 06:34:59 +0300

    Update Question4.py

--- Question4.py
+++ Question4.py
@@ -10,12 +10,13 @@ print(null_count)
 
 country_records = df.country.value_counts() # Count the number of records for each country
 groupped_records = df.groupby('country')['daily_vaccinations'] # Group all daily vaccinations by the 'country' column
+invalid_countries = [i[0] for i in pd.Series(groupped_records) if i[1].isna().sum() == len(i[1])] # Get all countries that have invalid records
 
 for record in groupped_records: # Iterate over groupped records   
-    if country_records[record[0]] < 2: # if the number of records is just 1, that means no valid records for that country
+    if record[0] in invalid_countries: # if all daily vaccination values of the current country are NaN, that means no valid records for that country
         df.loc[(df['country'] == record[0]) & (df.daily_vaccinations.isna()), 'daily_vaccinations'] = 0.0
     df.loc[(df['country'] == record[0]) & (df.daily_vaccinations.isna()), 'daily_vaccinations'] = groupped_records.min()[record[0]]
-
+    
     """ if the country matches the index - in .loc[] section - which is called "record", and if it has nan values,
     change it by the minimum value of the current index(country) """
 
